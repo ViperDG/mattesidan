8c81b1a9c07d0115c65107aab1b291c275e58588
Viper <certifag@gmail.com>
2020-04-20 13:38:01 +0200

    blabla

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("hejsan");
+document.getElementById("submit").onclick = () => {
+    const a = parseInt( document.getElementById("a").value);
+    const b = parseInt( document.getElementById("b").value);
+    const c = pyth(a, b);
+    document.getElementById("c").innerHTML = c;
+}
+
+function pyth(a, b){
+    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+}
